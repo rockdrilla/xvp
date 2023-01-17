6c2566e9fc5944b0bbd220d4cd94831be74566ca
Konstantin Demin <rockdrilla@gmail.com>
2023-01-17 12:38:13 +0300

    bump copyright years

--- xvp.cc
+++ xvp.cc
@@ -1,7 +1,7 @@
 /* xvp: simple (or sophisticated?) launcher
  *
  * SPDX-License-Identifier: Apache-2.0
- * (c) 2022, Konstantin Demin
+ * (c) 2022-2023, Konstantin Demin
  *
  * Example usage in shell scripts:
  *   xvp -u program /tmp/list
